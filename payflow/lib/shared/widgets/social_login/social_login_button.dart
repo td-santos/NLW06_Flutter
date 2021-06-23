b28a0e072b2ed05d09ce722f6e45ab5d6db964ec
Thiago Dantas <thiago_dantas411@hotmail.com>
2021-06-23 00:29:04 -0300

    commit layout splash, login

---
+++ payflow/lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+
+  final VoidCallback onTap;
+  const SocialLoginButton({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.shape,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(color: AppColors.stroke),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 16,),
+                  Image.asset(AppImages.google),
+                  SizedBox(width: 16,),
+                  Container(
+                    width: 1,
+                    height: 56,
+                    color: AppColors.stroke,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Entrar com Google',
+                    style: TextStyles.buttonGray,
+                  ),
+                ],
+              ),
+            ),
+            //Expanded(child: Container())
+          ],
+        ),
+      ),
+    );
+  }
+}
