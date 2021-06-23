fd3aaff7c076abbd1f8c8587f0596027b5084061
Thiago Dantas <thiago_dantas411@hotmail.com>
2021-06-23 14:56:25 -0300

    login google, bottom nav
    - add firebase ao projeto,
    - add google signin,
    - sistema de rotas,
    - add bottomnavigation bar na home

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -28,15 +32,13 @@ class _LoginPageState extends State<LoginPage> {
               color: AppColors.primary,
             ),
             Positioned(
-              left: 0,
-              right: 0,
-              top: 40,
-              child: Image.asset(
-                AppImages.person, 
-                width: 208,//size.width *0.4, 
-                height: 300//size.height * 0.5,
-              )),
-
+                left: 0,
+                right: 0,
+                top: 40,
+                child: Image.asset(AppImages.person,
+                    width: 208, //size.width *0.4,
+                    height: 300 //size.height * 0.5,
+                    )),
             Positioned(
               bottom: size.height * 0.18,
               left: 0,
@@ -47,20 +49,26 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Image.asset(AppImages.logomini),
                   Padding(
-                    padding: const EdgeInsets.only(left: 70,right: 70,top: 10),
-                    child: Text('Organize seus, boletos em um só lugar',
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, top: 10),
+                    child: Text(
+                      'Organize seus, boletos em um só lugar',
                       textAlign: TextAlign.center,
                       style: TextStyles.titleHome,
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 40,right: 40,top: 40),
-                    child: SocialLoginButton(onTap: (){},),
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: (){
+                        controller.googleSignin(context);
+                      },
+                    ),
                   )
                 ],
               ),
             ),
-            
           ],
         ),
       ),
---
+++ payflow/lib/shared/auth/auth_controller.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthController{
+
+  //var _isAuthenticated = false;
+
+  UserModel? _user;
+
+  UserModel get user => _user!;
+
+  void setUser(BuildContext context, UserModel? user){
+    if(user != null){
+
+      //_isAuthenticated = true;
+      saveUser(user);
+      _user = user;
+      Navigator.pushReplacementNamed(context, "/home");
+    }else{
+      //_isAuthenticated = false;
+      Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user)async{
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context)async{
+
+    final instance = await SharedPreferences.getInstance();
+
+    await Future.delayed(Duration(seconds: 2));
+
+    if(instance.containsKey('user')){
+
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));    
+      return;
+
+    }else{
+      setUser(context, null);
+    }
+    
+  }
+}
